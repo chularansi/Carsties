9fbc9d2f1cfd0d4f8f3750a6d04efa16c079d836
Chularansi Fernando <chularansifernando@yahoo.com>
2023-08-13 10:58:15 +0200

    Add the YARP Gateway Service.

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,35 @@
+﻿using MassTransit;
+using Contracts;
+using AuctionService.Data;
+using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Consumers
+{
+    public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+    {
+        private readonly AuctionDbContext dbContext;
+
+        public AuctionFinishedConsumer(AuctionDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task Consume(ConsumeContext<AuctionFinished> context)
+        {
+            Console.WriteLine("--> Consuming auction finished");
+
+            var auction = await dbContext.Auctions.FindAsync(Guid.Parse(context.Message.AuctionId));
+
+            if (context.Message.ItemSold)
+            {
+                auction.Winner = context.Message.Winner;
+                auction.SoldAmount = context.Message.Amount;
+            }
+
+            auction.Status = auction.SoldAmount > auction.ReservePrice
+                ? Status.Finished : Status.ReserveNotMet;
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -59,6 +59,22 @@ namespace IdentityService
                 ClientSecrets = { new Secret("NotASecret".Sha256()) },
                 AllowedGrantTypes = { GrantType.ResourceOwnerPassword },
             },
+
+            new Client
+            {
+                ClientId = "nextApp",
+                ClientName = "nextApp",
+                //ClientSecrets = {new Secret(config["ClientSecret"].Sha256())},
+                ClientSecrets = {new Secret("secret".Sha256())},
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false,
+                //RedirectUris = {config["ClientApp"] + "/api/auth/callback/id-server"},
+                RedirectUris = {"http://localhost:3000/api/auth/callback/id-server"},
+                AllowOfflineAccess = true,
+                AllowedScopes = {"openid", "profile", "auctionApp"},
+                AccessTokenLifetime = 3600*24*30,
+                //AlwaysIncludeUserClaimsInIdToken = true
+            }
         };
     }
 }
