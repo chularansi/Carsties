ee49813702d9d950f006dca4784557c25fa5315e
Chularansi Fernando <chularansifernando@yahoo.com>
2023-08-29 14:33:47 +0200

    Add Notification service, use SignalR

---
+++ src/NotificationService/Consumers/AuctionCreatedConsumer.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using MassTransit;
+using Microsoft.AspNetCore.SignalR;
+using NotificationService.Hubs;
+
+namespace NotificationService.Consumers
+{
+    public class AuctionCreatedConsumer : IConsumer<AuctionCreated>
+    {
+        private readonly IHubContext<NotificationHub> hubContext;
+
+        public AuctionCreatedConsumer(IHubContext<NotificationHub> hubContext)
+        {
+            this.hubContext = hubContext;
+        }
+
+        public async Task Consume(ConsumeContext<AuctionCreated> context)
+        {
+            Console.WriteLine("--> auction created message received");
+
+            await hubContext.Clients.All.SendAsync("AuctionCreated", context.Message);
+        }
+    }
+}
---
+++ src/NotificationService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using MassTransit;
+using Microsoft.AspNetCore.SignalR;
+using NotificationService.Hubs;
+
+namespace NotificationService.Consumers
+{
+    public class BidPlacedConsumer : IConsumer<BidPlaced>
+    {
+        private readonly IHubContext<NotificationHub> hubContext;
+
+        public BidPlacedConsumer(IHubContext<NotificationHub> hubContext)
+        {
+            this.hubContext = hubContext;
+        }
+
+        public async Task Consume(ConsumeContext<BidPlaced> context)
+        {
+            Console.WriteLine("--> bid placed message received");
+
+            await hubContext.Clients.All.SendAsync("BidPlaced", context.Message);
+        }
+    }
+}
